13506293c5519d50d747e59361d26569d7d541f2
wacho91 <criskol.71@gmail.com>
2024-08-26 11:08:15 -0500

    Finsishing  search & ShoppingCart

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 
+import { IoMdSearch } from "react-icons/io"
 import Logo from "../../assets/logo.png"
+import { FaCartShopping } from "react-icons/fa6"
 
 const Navbar = () => {
   return (
@@ -17,9 +19,9 @@ const Navbar = () => {
                     Shopsy
                 </a>
             </div>
-            {/*Search bar and order button*/}
-            <div>
-                <div className="group">
+            {/*Search bar*/}
+            <div className="flex justify-between items-center gap-4">
+                <div className=" relative group hidden sm:block">
                     <input 
                         type="text" 
                         placeholder="Search" 
@@ -27,8 +29,21 @@ const Navbar = () => {
                         duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none 
                         focus:border-1 focus:border-primary"
                     />
+                    <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3"/>
                 </div>
-            </div>
+                {/*order button*/}
+                <button 
+                    onClick={() => alert("Ordening not available yet")}
+                    className="bg-gradient-to-r from-primary to-secondary 
+                    transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
+                >
+                    <span className="group-hover:block hidden transition-all duration-200">Order</span>
+                    <FaCartShopping 
+                        className="text-xl text-white drop-shadow-sm cursor-pointer"
+                    />
+                </button>
+                {/*DarkMode*/}
+            </div>  
         </div>
       </div>
         {/*Lower  Navbar*/}
