b9569436e49e087a6f643f41b1fa9d47ccaeb22a
wacho91 <criskol.71@gmail.com>
2024-08-27 11:37:00 -0500

    Product Component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
 import Hero from "./component/Hero/Hero"
 import Navbar from "./component/Navbar/Navbar"
+import Products from "./component/Products/Products"
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  },[])
  
 
   return (
     <div>
       <Navbar />
       <Hero />
+      <Products />
     </div>
   )
 }
--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -57,17 +57,17 @@ const Hero = () => {
                         <div className="grid grid-cols-1 sm:grid-cols-2">
                             {/*Text Content Section*/}
                             <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                                <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                                <h1 data-aos="zoom-out" data-aos-duration="500" data-aos-once="true" className="text-5xl sm:text-6xl lg:text-7xl font-bold">
                                     {item.title}
                                 </h1>
-                                <p className="text-sm">{item.description}</p>
-                                <div>
+                                <p data-aos="fade-up" data-aos-duration="500" data-aos-delay="100" className="text-sm">{item.description}</p>
+                                <div data-aos="zoom-out" data-aos-duration="500" data-aos-delay="300">
                                     <button className="bg-gradient-to-r from-primary to-secondary 
                                     hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">Order Now</button>
                                 </div>
                             </div>  
                             {/*Image Section*/}
-                            <div className="order-1 sm:order-2">
+                            <div data-aos="fade-up" data-aos-once="true" className="order-1 sm:order-2">
                                 <div className="relative z-10">
                                     <img src={item.img} alt="" className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-120 object-contain mx-auto"/>
                                 </div>
